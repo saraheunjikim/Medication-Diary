810be9ab33843285721a8af99f6c6990c4649e02
Sarah <eunjiskim@outlook.com>
2020-12-08 17:58:15 -0600

    added
    fixed the function and new fuctions added

--- med_diary.py
+++ med_diary.py
@@ -16,34 +16,67 @@ def submit():
 
     return db.insertToDiary(currentTime, convertedMedName, convertedMedDosage, medNoteInput)
 
+def insert():
+    medNameInsInput = medNameIns.get()
+    medDosageInsInput = medDosageIns.get()
+    medDescInsInput = medDescIns.get()
+    medDiagnosisInsInput = medDiagnosisIns.get()
+    medDiagnosisDescInsInput = medDiagnosisDescIns.get()
+    
+    print(medNameInsInput, medDosageInsInput, medDescInsInput, medDiagnosisInsInput, medDiagnosisDescInsInput)
 
 # Interface
 master = tk.Tk()
-master.geometry("350x200")
 master.title("Med Diary")
 
 master.configure(background="lemon chiffon")
 
 
 tk.Label(master, text=f"{dt.datetime.now():%b %d, %H:%M}", fg="white", bg="black", font=("Verdana", 11)).grid(row=0)
+tk.Label(master, text="Insert Medication", fg="white", bg="black", font=("Verdana", 11)).grid(row=5)
 
 tk.Label(master, text="Medication Name", bg="lemon chiffon", font=("Verdana", 8)).grid(row=1)
-tk.Label(master, text="Medication Dosage", bg="lemon chiffon", font=("Verdana", 8)).grid(row=2)
+tk.Label(master, text="Medication Dosage (mg)", bg="lemon chiffon", font=("Verdana", 8)).grid(row=2)
 tk.Label(master, text="Medication Note", bg="lemon chiffon", font=("Verdana", 8)).grid(row=3)
+tk.Label(master, text="Medication Name", bg="lemon chiffon", font=("Verdana", 8)).grid(row=6)
+tk.Label(master, text="Medication Dosage (mg)", bg="lemon chiffon", font=("Verdana", 8)).grid(row=7)
+tk.Label(master, text="Medication Description", bg="lemon chiffon", font=("Verdana", 8)).grid(row=8)
+tk.Label(master, text="Medication Used For", bg="lemon chiffon", font=("Verdana", 8)).grid(row=9)
+tk.Label(master, text="Diagnosis Description", bg="lemon chiffon", font=("Verdana", 8)).grid(row=10)
+
+
+
 
 medName = tk.Entry(master)
 medDosage = tk.Entry(master)
 medNote = tk.Text(master, wrap="word", width=20, height=5)
+medNameIns = tk.Entry(master)
+medDosageIns = tk.Entry(master)
+medDescIns = tk.Entry(master)
+medDiagnosisIns = tk.Entry(master)
+medDiagnosisDescIns = tk.Entry(master)
+
 
 medName.grid(row=1, column=1, pady=3)
 medDosage.grid(row=2, column=1, pady=3)
 medNote.grid(row=3, column=1, pady=3)
+medNameIns.grid(row=6, column=1, pady=3)
+medDosageIns.grid(row=7, column=1, pady=3)
+medDescIns.grid(row=8, column=1, pady=3)
+medDiagnosisIns.grid(row=9, column=1, pady=3)
+medDiagnosisDescIns.grid(row=10, column=1, pady=3)
 
 medName.config({"background": "ivory"})
 medDosage.config({"background": "ivory"})
 medNote.config({"background": "ivory"})
+medNameIns.config({"background": "ivory"})
+medDosageIns.config({"background": "ivory"})
+medDiagnosisIns.config({"background": "ivory"})
+medDiagnosisDescIns.config({"background": "ivory"})
 
 Submit = tk.Button(master, text='Submit', bg="blue", fg="ivory", font=("Verdana", 8),
-                   command=submit, height=5, width=5).grid(row=3, column=3)
+                   command=submit, height=1, width=8).grid(row=4, column=1)
+Insert = tk.Button(master, text='Insert', bg="blue", fg="ivory", font=("Verdana", 8),
+                   command=insert, height=1, width=8).grid(row=11, column=1)
 
 master.mainloop()
--- med_diary_database.py
+++ med_diary_database.py
@@ -4,7 +4,7 @@ import mysql.connector
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="password",
+    password="Dmdenl1004!",
     database="med_diary"
 )
 
@@ -30,23 +30,43 @@ def convertMedNameToKey(medName):
 
     return False
 
-
 def convertMedDosageToKey(medDosage):
     medDict = {}
-    medDosage = int(''.join(filter(str.isdigit, medDosage)))
+    medDosage = str(medDosage)
+
+    try:
+        newMedDosage = int(''.join(filter(str.isdigit, medDosage)))
+    except ValueError or TypeError:
+        newMedDosage = "0mg"
+
     myCursor.execute("SELECT dosageId, dosageDesc FROM dosage")
     for (dosageId, dosageDesc) in myCursor:
         medDict[dosageId] = dosageDesc
 
     for key, value in medDict.items():
-        if value == medDosage:
+        if value == newMedDosage:
             return key
 
     return False
 
+def insertDiagnosis(medDiagnosis, medDiagnosisDesc):
+    myCursor.execute("INSERT INTO diagnosis (diagnosisName, diagnosisDesc) VALUES (%s, %s)",
+                     (medDiagnosis, medDiagnosisDesc))
+    return db.commit()
 
+def insertDosage(medDosage):
+    try:
+        newMedDosage = int(''.join(filter(str.isdigit, medDosage)))
+    except ValueError or TypeError:
+        newMedDosage = "0mg"
 
+    if not convertMedDosageToKey(newMedDosage):
+        myCursor.execute("INSERT INTO dosage (dosageDesc) VALUES (%s)", (newMedDosage,))
+        return db.commit()
 
+    return convertMedDosageToKey(newMedDosage)
 
+def insertMedication(medName, medDesc, medDiagnosis):
+    pass
 
 
